2a891222fefdbe2ce4dfaa2c2335c91a365563dd
IsaacBrandaoOSF <isaac.brandaoe@osf.digital>
2023-04-15 00:18:19 -0300

    feat(app): try to import all components in js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // LOCAL COMPONENTS
 
+import AFRAME from "aframe";
+import eventSet from "aframe-event-set-component";
+import template from "aframe-template-component";
+import layout from "aframe-layout-component";
+import proxyEvent from "aframe-proxy-event-component";
+
 import menu from "./components/menu/menu.js";
 import camera from "./components/camera/camera.js";
 import illumination from "./components/illumination/illumination.js";
