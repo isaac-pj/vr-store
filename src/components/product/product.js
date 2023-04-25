b6b8cc412fa8a2f711e25de7e83791558791305e
IsaacBrandaoOSF <isaac.brandaoe@osf.digital>
2023-04-25 15:13:29 -0300

    feat(product): add selector to set target

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -2,68 +2,59 @@ import { waitFor } from "../../utils/general";
 
 AFRAME.registerComponent("wrapper", {
   schema: {
-    selector: { type: "string", default: null },
+    selector: { type: "string", default: "" },
     pivot: { type: "string", default: "center" },
-    // pivot: { type: "vec3", default: { x: 0, y: 0, z: 0 } },
   },
   init: function () {
     const self = this;
     const { pivot, selector } = this.data;
 
-    this.el.firstElementChild.addEventListener(
-      "model-loaded",
-      async function () {
-        await waitFor(1000);
-        const { boxMin, boxMax, boxCenter } = this.components["aabb-collider"];
+    const wrapper = this.el;
+    const target = selector ? document.querySelector(selector) : wrapper?.firstElementChild;
+    const isGltfModel = target?.hasAttribute("gltf-model");
 
-        const width = Number((boxMax.x - boxMin.x).toFixed(3));
-        const height = Number((boxMax.y - boxMin.y).toFixed(3));
-        const depth = Number((boxMax.z - boxMin.z).toFixed(3));
+    if (!target) return;
 
-        const offset = height / 2;
-        const offsetReverse = (height / 2) * -1;
+    async function boundingBoxUpdate() {
+      await waitFor(100);
+      const { boxMin, boxMax } = this.components["aabb-collider"];
 
-        const geometry = self.el.getAttribute("geometry");
-        const position = self.el.getAttribute("position");
-        const pos = this.getAttribute("position");
+      const width = Number((boxMax.x - boxMin.x).toFixed(3));
+      const height = Number((boxMax.y - boxMin.y).toFixed(3));
+      const depth = Number((boxMax.z - boxMin.z).toFixed(3));
 
-        self.el.setAttribute("geometry", { ...geometry, width, height, depth });
+      const offset = height / 2;
 
-        switch (pivot) {
-          case "top":
-            self.el.setAttribute("position", { ...position, y: offsetReverse });
-            this.setAttribute("position", { ...pos, y: offset });
-            break;
-          case "center":
-            break;
-          case "bottom":
-            self.el.setAttribute("position", { ...position, y: offset });
-            this.setAttribute("position", { ...pos, y: offsetReverse });
-            break;
-          default:
-            break;
-        }
+      const wrapperGeometry = wrapper.getAttribute("geometry");
+      wrapper.setAttribute("geometry", { ...wrapperGeometry, width, height, depth });
+      self.handlePivot(pivot, wrapper, target, offset);
+    }
 
-        // console.log(self.data);
-        // console.log(this.components["aabb-collider"]);
+    if (target && isGltfModel) {
+      target.addEventListener("model-loaded", boundingBoxUpdate);
+    }
+  },
+  handlePivot: (pivot, wrapper, target, offset) => {
+    const offsetReverse = offset * -1;
+    const wrapperPosition = wrapper.getAttribute("position");
+    const targetPosition = target.getAttribute("position");
 
-        // console.log(self.el.object3D);
-        // console.log(THREE);
-        // console.log(self.el.object3D);
-
-        // return;
-        // console.log(
-        //   this.id,
-        //   `${width}${width < 1 ? "cm" : "m"}`,
-        //   `${height}${height < 1 ? "cm" : "m"}`,
-        //   `${depth}${depth < 1 ? "cm" : "m"}`,
-        //   `${offset}${offset < 1 ? "cm" : "m"}`
-        // );
-        //     // console.log(geometry);
-        //     // console.log(p1, p2);
-        //     // console.log(this, self.el);
-      }
-    );
+    switch (pivot) {
+      case "top":
+        wrapper.setAttribute("position", { ...wrapperPosition, y: offsetReverse });
+        target.setAttribute("position", { ...targetPosition, y: offset });
+        break;
+      case "bottom":
+        wrapper.setAttribute("position", { ...wrapperPosition, y: offset });
+        target.setAttribute("position", { ...targetPosition, y: offsetReverse });
+        break;
+      case "center":
+        // no offset
+        break;
+      default:
+        // no offset
+        break;
+    }
   },
 });
 
