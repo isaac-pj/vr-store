bda886ef26f3ac01eb0702a40e76f410c18b1838
IsaacBrandaoOSF <isaac.brandao@osf.digital>
2023-05-13 08:48:43 -0300

    chore(assets): change fonts folder

--- src/components/commons/montserrat.js
+++ src/components/commons/montserrat.js
@@ -1,38 +1,38 @@
 const variants = {
   100: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Thin-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Thin-msdf.json",
     italic: "",
   },
   200: {
-    normal: "../assets/fonts/Montserrat/Montserrat-ExtraLight-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-ExtraLight-msdf.json",
     italic: "",
   },
   300: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Light-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Light-msdf.json",
     italic: "",
   },
   400: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Regular-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Regular-msdf.json",
     italic: "",
   },
   500: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Medium-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Medium-msdf.json",
     italic: "",
   },
   600: {
-    normal: "../assets/fonts/Montserrat/Montserrat-SemiBold-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-SemiBold-msdf.json",
     italic: "",
   },
   700: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Bold-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Bold-msdf.json",
     italic: "",
   },
   800: {
-    normal: "../assets/fonts/Montserrat/Montserrat-ExtraBold-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-ExtraBold-msdf.json",
     italic: "",
   },
   900: {
-    normal: "../assets/fonts/Montserrat/Montserrat-Black-msdf.json",
+    normal: "../assets/fonts/msdf/Montserrat/Montserrat-Black-msdf.json",
     italic: "",
   },
 };
