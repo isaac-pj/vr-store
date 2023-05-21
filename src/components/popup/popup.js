c4c58b78239ca7f26c79c637527ea50db9e4b9d5
IsaacBrandaoOSF <isaac.brandao@osf.digital>
2023-05-20 23:49:30 -0300

    feat(primitives): add popup primitive

---
+++ src/components/popup/popup.js
@@ -0,0 +1,8 @@
+AFRAME.registerPrimitive("c-popup", {
+  defaultComponents: {
+    template: {
+      src: "./components/popup/popup.html",
+      type: "mustache",
+    },
+  },
+});
--- src/index.js
+++ src/index.js
@@ -10,18 +10,28 @@ import "aframe-troika-text";
 
 // LOCAL COMPONENTS
 
+import lookTo from "./components/commons/look-to.js";
+import montserrat from "./components/commons/montserrat.js";
+import router from "./components/commons/router.js";
+import wrapper from "./components/commons/wrapper.js";
+import children from "./components/commons/children";
+
+// EXTENDS COMPONENT
+
+import light from "./components/commons/light.js";
+import text from "./components/text/text.js";
+
+// CUSTOM PRIMITIVES
+
 import menu from "./components/menu/menu.js";
 import ambience from "./components/ambience/ambience.js";
 import product from "./components/product/product.js";
-import light from "./components/commons/light.js";
-import router from "./components/commons/router.js";
-import lookTo from "./components/commons/look-to.js";
-import montserrat from "./components/commons/montserrat.js";
-import database from "./data/database.js";
-import text from "./components/text/text.js";
-import children from "./components/commons/children";
-import wrapper from "./components/commons/wrapper.js";
 import mybox from "./components/mybox/mybox.js";
+import popup from "./components/popup/popup.js";
+
+// DATABASE COMPONENT
+
+import database from "./data/database.js";
 
 async function start() {
   // Put your code here
