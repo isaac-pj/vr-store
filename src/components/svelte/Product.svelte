96b24de6935cf8c215ea00798ad5e95275ed0e6d
isaac-pj <isaac-pj@hotmail.com>
2023-05-26 19:57:01 -0300

    feat(primitives): migrate product component

---
+++ src/components/svelte/Product.svelte
@@ -0,0 +1,36 @@
+<script lang="js">
+  export let model;
+  export let event;
+  export let target;
+  export let position;
+  export let rotation;
+  export let pivot = "center";
+  export let debug = false;
+
+  $: model = model;
+  $: event = event;
+  $: target = target;
+  $: position = position;
+  $: rotation = rotation;
+  $: pivot = pivot;
+  $: debug = debug;
+</script>
+
+<a-entity
+  {position}
+  {rotation}
+  id={`box-${model}`}
+  wrapper={{ pivot }}
+  geometry="primitive: box;"
+  material={{ color: "#FAC200", wireframe: true, visible: debug }}
+  event-set__ready="_event: wrapped; class: cursor;"
+  proxy-event={{ event, to: `:has(> #box-${model}) ${target}` }}
+>
+  <a-entity
+    id={model}
+    gltf-model={`#${model}-3d`}
+    shadow="receive: true; cast: true"
+  />
+
+  <slot />
+</a-entity>
