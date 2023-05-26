c621106e826fb03d82709229c0f7528cfc9de30e
isaac-pj <isaac-pj@hotmail.com>
2023-05-26 20:15:35 -0300

    feat(spaces): add a product list space

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,26 @@
 export const routes = [
-  { key: "livingRoomRoute", path: "/living-room", src: "./spaces/living-room.html", type: "mustache" },
-  { key: "productDetailsRoute", path: "/product-details", src: "./spaces/product-details.html", type: "mustache" },
-  { key: "experimentsRoute", path: "/experiments", src: "./spaces/experiments.html", type: "mustache" },
+  {
+    key: "livingRoomRoute",
+    path: "/living-room",
+    src: "./spaces/living-room.html",
+    type: "mustache",
+  },
+  {
+    key: "productDetailsRoute",
+    path: "/product-details",
+    src: "./spaces/product-details.html",
+    type: "mustache",
+  },
+  {
+    key: "experimentsRoute",
+    path: "/experiments",
+    src: "./spaces/experiments.html",
+    type: "mustache",
+  },
+  {
+    key: "productListRoute",
+    path: "/list",
+    src: "./spaces/product-list.html",
+    type: "mustache",
+  },
 ];
