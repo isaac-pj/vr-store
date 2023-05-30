83054bed6b531a8ef2724bbdc14f1145f38f82a7
isaac-pj <isaac-pj@hotmail.com>
2023-05-30 10:25:19 -0300

    feat(menu): some adjusts for umbrella menu

--- src/components/svelte/Menu.svelte
+++ src/components/svelte/Menu.svelte
@@ -12,37 +12,55 @@
   radius="198"
   opacity="0"
   theta-length="0"
-  animation__show="property: opacity; to: 1; dur: 300; startEvents: mouseenter"
-  animation__hide="property: opacity; to: 0; dur: 300; startEvents: mouseleave"
-  animation__open="property: theta-length; to: 65; dur: 300; startEvents: mouseenter"
-  animation__close="property: theta-length; to: 0; dur: 300; startEvents: mouseleave"
-  proxy-event__show="event: animationcomplete__open; to: #circleLayout; as: mouseenter; captureBubbles: true;"
-  proxy-event__hide="event: mouseleave; to: #circleLayout; captureBubbles: true;"
->
+
+  animation__show="property: opacity; to: 1; dur: 300; startEvents: open"
+  animation__hide="property: opacity; to: 0; dur: 300; startEvents: close"
+
+  animation__open="property: theta-length; to: 65; dur: 300; startEvents: open"
+  animation__close="property: theta-length; to: 0; dur: 300; startEvents: close"
+  
+  proxy-event__show="event: animationcomplete__open; to: #circleLayout; as: show;"
+  proxy-event__hide="event: animationbegin__close; to: #circleLayout; as: hide;"
+  
+  >
+  <!-- proxy-event__hide="event: mouseleave; to: #circleLayout; as: hide; captureBubbles: true;" -->
+
+  <!-- proxy-event__show="event: animationcomplete__open; to: #circleLayout; as: mouseenter;" -->
+  <!-- proxy-event__hide="event: mouseleave; to: #circleLayout; captureBubbles: true;" -->
+  
   <a-sphere
     id="umbrellaMenuTrigger"
     class="cursor"
     material="side: back; shader: flat; wireframe: true; color: #f00; visible: false;"
     theta-length="59"
     radius="13"
-  >
-    <!-- proxy-event__mouseenter="to: #umbrellaMenu;"
-  proxy-event__mouseleave="to: #umbrellaMenu;"
-  proxy-event__hide="event: mouseleave; to: #circleLayout;" -->
+    proxy-event__open="event: mouseenter; to: #umbrellaMenu; as: open; captureBubbles: true; bubbles: true;"
+    proxy-event__close="event: mouseleave; to: #umbrellaMenu; as: close; captureBubbles: true; bubbles: true;"
+    >
+    
+    <!-- proxy-event__hide="event: mouseleave; to: #circleLayout; as: hide; captureBubbles: true;" -->
 
+    <!-- proxy-event__open="event: mouseenter; to: #umbrellaMenu; as: open" -->
+    <!-- proxy-event__close="event: mouseleave; to: #umbrellaMenu; as: close" -->
+    <!-- proxy-event__hide="event: mouseleave; to: #circleLayout; as: hide; captureBubbles: true;" -->
+    
+    <!-- proxy-event__show="event: mouseenter; to: #circleLayout; as: show" -->
+    <!-- proxy-event__hide="event: mouseleave; to: #circleLayout; as: hide" -->
+    
     <!-- proxy-event__show="event: mouseenter; to: #circleLayout;" -->
-    <!-- proxy-event__hide="event: animationbegin; to: #circleLayout; as: mouseleave;" -->
+    <!-- proxy-event__hide="event: mouseleave; to: #circleLayout;" -->
 
     <!-- class="cursor" -->
     <!-- material="color: blue; wireframe: true; open-ended: true;" -->
     <!-- geometry="primitive: cylinder; radius: 11;" -->
+
     <a-entity
       id="circleLayout"
       layout="type: circle; radius: 10; plane: xz;"
       position="0 7 0"
       visible="false"
-      event-set__show="_event: mouseenter; visible: true; _delay: 100;"
-      event-set__hide="_event: mouseleave; visible: false;"
+      event-set__show="_event: show; visible: true; _delay: 100;"
+      event-set__hide="_event: hide; visible: false;"
     >
       <MenuItem label="Celulares" ref="" />
       <MenuItem label="Brinquedos" ref="" />
