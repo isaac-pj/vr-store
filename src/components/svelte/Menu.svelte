ed775772b30279d4df696cb6d00025e1ea4468aa
isaac-pj <isaac-pj@hotmail.com>
2023-05-30 12:35:46 -0300

    feat(menu): some adjusts for umbrella menu

--- src/components/svelte/Menu.svelte
+++ src/components/svelte/Menu.svelte
@@ -1,6 +1,13 @@
 <script>
   import MenuItem from "./MenuItem.svelte";
   import MenuRing from "./MenuRing.svelte";
+
+  let circleLayout;
+
+  const handleMouseLeave = ({ target }) => {
+    if (target?.id !== "umbrellaTrigger") return;
+    circleLayout.emit("hide");
+  };
 </script>
 
 <MenuRing />
@@ -17,7 +24,6 @@
   animation__open="property: theta-length; to: 65; dur: 300; startEvents: mouseenter"
   animation__close="property: theta-length; to: 0; dur: 300; startEvents: mouseleave"
   event-set__show="_event: animationcomplete__open; _target: #circleLayout; visible: true; _delay: 100;"
-  event-set__hide="_event: animationbegin__close; _target: #circleLayout; visible: false;"
 >
   <a-sphere
     id="umbrellaTrigger"
@@ -25,13 +31,19 @@
     material="side: back; shader: flat; wireframe: true; color: #f00; visible: false;"
     theta-length="59"
     radius="13"
+    on:raycaster-intersected-cleared={handleMouseLeave}
   >
     <a-entity
+      bind:this={circleLayout}
       id="circleLayout"
       layout="type: circle; radius: 10; plane: xz;"
       position="0 7 0"
       visible="false"
+      event-set__show="_event: show; visible: true; _delay: 100;"
+      event-set__hide="_event: hide; visible: false;"
     >
+      <!-- event-set__show="_event: mouseenter; visible: true; _delay: 100;" -->
+      <!-- event-set__hide="_event: mouseleave; visible: false;" -->
       <MenuItem label="Celulares" ref="" />
       <MenuItem label="Brinquedos" ref="" />
       <MenuItem label="Games" ref="" />
