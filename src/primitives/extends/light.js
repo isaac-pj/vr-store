8f45a0d0c324e337126656da4f8d021418dbea2c
isaac-pj <isaac-pj@hotmail.com>
2023-06-02 12:02:29 -0300

    feat(light): some adjusts for light and shadow

--- src/primitives/extends/light.js
+++ src/primitives/extends/light.js
@@ -3,11 +3,15 @@ export default AFRAME.registerPrimitive("c-light", {
     light: {
       castShadow: true,
       shadowBias: -0.001,
+      shadowCameraFar: 200,
     },
   },
   mappings: {
     ...AFRAME.primitives.primitives["a-light"].mappings,
     castshadow: "light.castShadow",
     bias: "light.shadowBias",
+    radius: "light.shadowRadius",
+    automatic: "light.shadowCameraAutomatic",
+    "shadow-camera-far": "light.shadowCameraFar",
   },
 });
