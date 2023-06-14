22646ecd947fcb3a7598a11e2ce011f8e3704276
isaac-pj <isaac-pj@hotmail.com>
2023-06-14 16:46:19 -0300

    feat(primitives): add a-bevel primitive

--- src/components/svelte/experiments/EX07.svelte
+++ src/components/svelte/experiments/EX07.svelte
@@ -1,5 +1,9 @@
 <a-entity
-  position="0 0 -3"
-  geometry="primitive: bevel; bevelEnabled: false;"
-  material="color: #1A91FE; wireframe: true; shader: flat;"
+  position="0 0.5 -3"
+  geometry="primitive: bevel; bevelEnabled: true;"
+  material="color: #1A91FE;"
 />
+
+<a-bevel position="-2 0.5 -3" color="#f54400" bevel="0.2" />
+
+<!-- material="color: #1A91FE; wireframe: true; shader: flat;" -->
