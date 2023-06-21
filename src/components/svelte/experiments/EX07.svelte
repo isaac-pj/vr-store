71b85e2608956437953aaf77963150864269b7f6
isaac-pj <isaac-pj@hotmail.com>
2023-06-20 22:24:27 -0300

    feat(experiments): test origin component

--- src/components/svelte/experiments/EX07.svelte
+++ src/components/svelte/experiments/EX07.svelte
@@ -12,10 +12,19 @@
 
 <a-bevel position="-2 0.5 -3" color="#f54400" bevel="0.2" />
 
+<!-- pivot="origin: -0.5 -0.5 0;" -->
 <a-rounded
-  pivot="origin: -0.5 -0.5 0;"
+  origin="-0.5 -0.5 0;"
   position="0 2 -3"
   color="#ffffff"
   radius="0.3 0.3 0.3 0.1"
   material="color: #6020FE; wireframe: true;"
-/>
+>
+  <a-plane
+    width="0.5"
+    height="0.5"
+    origin="0.25 0.25 0"
+    color="blue"
+    position="1 1 0"
+  />
+</a-rounded>
