578ba3927ea694b066889317403b7168670758ce
isaac-pj <isaac-pj@hotmail.com>
2023-06-21 01:26:12 -0300

    feat(components): add dimensions in button

--- src/components/svelte/experiments/Button.svelte
+++ src/components/svelte/experiments/Button.svelte
@@ -4,6 +4,8 @@
   export let color = "#272727";
   export let background = "#FAC200";
   export let position = "0 0 0";
+  export let width = 0.8;
+  export let height = 0.2;
   export let onClick = () => {};
 
   $: id = id;
@@ -11,19 +13,21 @@
   $: color = color;
   $: background = background;
   $: position = position;
+  $: width = width;
+  $: height = height;
   $: onClick = onClick;
 </script>
 
 <a-plane
   {id}
   {position}
+  {width}
+  {height}
   color={background}
   text={`value: ${label.toUpperCase()}; width: 1.6; align: center; letterSpacing: 10px; yOffset: 0.01; color: ${color};`}
   shadow="cast: true; receive: true;"
   material="shader: flat"
   montserrat="weight: 700"
-  width="0.8"
-  height="0.2"
   on:click={onClick}
   on:keydown={() => {}}
 />
