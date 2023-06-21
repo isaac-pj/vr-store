ea27c6874115dfe6545efb5806432088fff119ef
isaac-pj <isaac-pj@hotmail.com>
2023-06-20 21:19:39 -0300

    feat(product): add flag to active or inactive controll

--- src/components/svelte/Product.svelte
+++ src/components/svelte/Product.svelte
@@ -7,6 +7,7 @@
   export let pivot = "center";
   export let debug = false;
   export let wrapper = true;
+  export let active = true;
 
   $: model = model;
   $: event = event;
@@ -16,6 +17,7 @@
   $: pivot = pivot;
   $: debug = debug;
   $: wrapper = wrapper;
+  $: active = active;
 </script>
 
 <a-entity
@@ -25,11 +27,12 @@
   wrapper={{ pivot, enabled: wrapper }}
   geometry="primitive: box;"
   material={{ color: "#FAC200", wireframe: true, visible: debug }}
-  event-set__ready="_event: wrapped; class: cursor active;"
+  event-set__ready={`_event: wrapped; class: ${active ? "cursor active" : ""};`}
   proxy-event={{ event, to: `:has(> #box-${model}) ${target}` }}
 >
   <a-entity
     id={model}
+    class={!active ? "cursor" : ""}
     gltf-model={`#${model}-3d`}
     shadow="receive: true; cast: true"
   />
