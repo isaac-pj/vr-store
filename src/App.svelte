5163adfbddbbbc30b0ead1d29baaacb8f969840b
isaac-pj <isaac-pj@hotmail.com>
2023-07-02 21:29:00 -0300

    chore(prettier): change line length

--- src/App.svelte
+++ src/App.svelte
@@ -18,25 +18,13 @@
     />
 
     <!-- 3D OBJECTS -->
-    <a-asset-item
-      id="armchair-3d"
-      src="./build/assets/models/scene_01/Armchair.gltf"
-    />
+    <a-asset-item id="armchair-3d" src="./build/assets/models/scene_01/Armchair.gltf" />
     <a-asset-item id="lamp-3d" src="./build/assets/models/scene_01/Lamp.gltf" />
     <a-asset-item id="rug-3d" src="./build/assets/models/scene_01/Rug.gltf" />
-    <a-asset-item
-      id="table-3d"
-      src="./build/assets/models/scene_01/Table.gltf"
-    />
+    <a-asset-item id="table-3d" src="./build/assets/models/scene_01/Table.gltf" />
     <a-asset-item id="wall-3d" src="./build/assets/models/scene_01/Wall.gltf" />
-    <a-asset-item
-      id="painting-3d"
-      src="./build/assets/models/scene_01/Painting.gltf"
-    />
-    <a-asset-item
-      id="plant-3d"
-      src="./build/assets/models/scene_01/Plant.gltf"
-    />
+    <a-asset-item id="painting-3d" src="./build/assets/models/scene_01/Painting.gltf" />
+    <a-asset-item id="plant-3d" src="./build/assets/models/scene_01/Plant.gltf" />
 
     <!-- AUDIOS -->
     <audio
@@ -54,17 +42,12 @@
     />
 
     <!-- FONTS -->
-    <a-asset-item
-      id="material-icons-font"
-      src="./build/assets/fonts/icons/MaterialIconsOutlined-Regular.otf"
-    />
+    <a-asset-item id="material-icons-font" src="./build/assets/fonts/icons/MaterialIconsOutlined-Regular.otf" />
 
     {#each fonts.montserrat.files as { name, format } (name)}
       <a-asset-item
         id={name?.toLowerCase()}
-        src={`${fonts.montserrat.path}${name}.${
-          format || fonts.montserrat.format
-        }`}
+        src={`${fonts.montserrat.path}${name}.${format || fonts.montserrat.format}`}
       />
     {/each}
   </a-assets>
